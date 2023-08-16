14ce6f891f12336481cb7d4c078f4cd30bc2f771
Sobhit Sharma <sobhit.sharma@kaleyra.com>
2023-08-16 20:39:16 +0530

    Day4 fixture and alias in cypress

--- cypress/e2e/CypressDay4/claimCRM.cy.js
+++ cypress/e2e/CypressDay4/claimCRM.cy.js
@@ -1,11 +1,19 @@
 describe('Buzz Tests on Orange HRM Application', () => {
 
     beforeEach('Before Tests', () => {
-      cy.loginHRM('Admin','admin123')
-      cy.get('a[href="/web/index.php/claim/viewClaimModule"]').as('ClaimModule') // creating alisas
+      // Load fixture data
+    cy.fixture('example.json').then((testdata) => {
+      const { baseUrlUsername, baseUrlPassword } = testdata;
+      // Fill in the login form
+      cy.loginHRM(baseUrlUsername,baseUrlPassword)
+    })
+      
     })
 
     it('Verify user able to post in newsfeed of Buzz Module', () => {
+        
+        cy.get('a[href="/web/index.php/claim/viewClaimModule"]').as('ClaimModule') // creating alisas
+
         cy.get('@ClaimModule').should('be.visible').click() //using alisas
         cy.get('button[type="submit"]').click
     })
